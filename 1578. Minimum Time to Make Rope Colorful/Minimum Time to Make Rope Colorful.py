04853a9b54ac52b10c42df25cdf36f948e911128
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2022-10-03 21:48:10 +0530

    Create Minimum Time to Make Rope Colorful.py

---
+++ 1578. Minimum Time to Make Rope Colorful/Minimum Time to Make Rope Colorful.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minCost(self, s: str, cost: List[int]) -> int:
+        ans = prev = 0 # index of previously retained letter 
+        for i in range(1, len(s)): 
+            if s[prev] != s[i]: prev = i
+            else: 
+                ans += min(cost[prev], cost[i])
+                if cost[prev] < cost[i]: prev = i
+        return ans 
