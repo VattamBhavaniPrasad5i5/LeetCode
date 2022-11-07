3d32dd80c5cd922a8e34e0a308c6f18fd9412ff0
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2022-11-08 00:16:00 +0530

    Time: 365 ms (61.93%), Space: 15.6 MB (65.19%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,8 @@
+class Solution:
+    def moveZeroes(self, nums):
+        zero = 0  
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
+        
