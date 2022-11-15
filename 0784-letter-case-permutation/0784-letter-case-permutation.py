f991dec8fab6ea3a5817fa7f2e61f9d43c553862
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2022-11-15 17:45:21 +0530

    Time: 131 ms (39.55%), Space: 14.6 MB (83.48%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -0,0 +1,9 @@
+class Solution:
+    def letterCasePermutation(self, S):
+        res = ['']
+        for ch in S:
+            if ch.isalpha():
+                res = [i+j for i in res for j in [ch.upper(), ch.lower()]]
+            else:
+                res = [i+ch for i in res]
+        return res
