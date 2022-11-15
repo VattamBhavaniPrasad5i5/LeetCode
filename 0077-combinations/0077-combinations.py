49bc46a0234da33c6aa0ef23650a8535cb7af6e5
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2022-11-15 17:40:31 +0530

    Time: 813 ms (55.28%), Space: 15.9 MB (52.76%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combine(self, n, k):
+        sol=[]
+        def backtrack(remain,comb,nex):
+			# solution found
+            if remain==0:
+                sol.append(comb.copy())
+            else:
+				# iterate through all possible candidates
+                for i in range(nex,n+1):
+					# add candidate
+                    comb.append(i)
+					#backtrack
+                    backtrack(remain-1,comb,i+1)
+					# remove candidate
+                    comb.pop()
+            
+        backtrack(k,[],1)
+        return sol
