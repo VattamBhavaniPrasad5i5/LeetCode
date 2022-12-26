85ed0fef2377549b5427bb396ccff96585306c6a
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2022-12-26 20:39:37 +0530

    Time: 555 ms (76.94%), Space: 15.2 MB (49.64%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        l=len(nums)
+        i = curr_max=0
+        while(i<l - 1):
+            curr_max=max(curr_max,i+nums[i])
+            if i<curr_max:
+                i+=1
+            else:
+                return False
+        print(i)
+        return True
+                
+            
+            
+        
+        
+            
+            
+        
