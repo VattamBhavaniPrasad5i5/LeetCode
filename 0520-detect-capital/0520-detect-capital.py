a532d245fdc1f76d0e951f41b85951166bc87e78
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2023-01-02 11:14:41 +0530

    Time: 36 ms (79.09%), Space: 13.8 MB (96.54%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,8 +1,14 @@
+import time
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
+        s_time=time.time()
         w=word.upper()
         w1=word.lower()
         w2=word.capitalize()
         if w==word or w1==word or w2==word:
-            return True
+             endtime=time.time()
+             print(endtime-s_time)
+             return True
+        endtime=time.time()
+        print(endtime-s_time)
         return False
