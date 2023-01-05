cce3f17831d3c7510a55bb4b45e8074494551701
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2023-01-05 10:34:33 +0530

    Time: 2866 ms (10.78%), Space: 29.2 MB (11.27%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minimumRounds(self, tasks: List[int]) -> int:
+        dic={}
+        re=0
+        flag=0
+        for i in range(len(tasks)):
+            if tasks[i] in dic:
+                dic[tasks[i]]+=1
+            else:
+                dic[tasks[i]]=1
+        p=0
+        print(dic)
+        for i in dic:
+            #print(dic[i],i)
+            if dic[i]==2 or dic[i]==3:
+                re+=1
+            elif dic[i]==1:
+                flag=1
+                break
+            else:
+                while dic[i]>0:
+                    
+                    re+=1
+                    #print(dic[i])
+                    dic[i]=dic[i]-3
+                    #print(dic[i])
+        if flag==0:
+            return re
+        else:
+            return -1
+                
+                
