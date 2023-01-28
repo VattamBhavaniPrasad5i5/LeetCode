a182e8fb1216ca6cf968484d74629018bc9e1ed8
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2023-01-28 11:26:35 +0530

    Time: 76 ms (54.96%), Space: 16.6 MB (7.12%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,6 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        d={}
         n=set(nums)
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
         for i in n:
-            if nums.count(i)==1:
+            if d[i]==1:
                 return i
