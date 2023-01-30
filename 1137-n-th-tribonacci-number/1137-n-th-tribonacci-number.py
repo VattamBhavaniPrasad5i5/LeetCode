991cf9e0b8bb79bc1ee40884c6c266abe2638390
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2023-01-30 10:26:37 +0530

    Time: 36 ms (51.68%), Space: 13.8 MB (54.01%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -5,3 +5,13 @@ class Solution:
             t[i]=t[i-1]+t[i-2]+t[i-3]
         return t[n]
         
+        # def tribonacci(n):
+        #     if n==0:
+        #         return 0
+        #     elif n==1:
+        #         return 1
+        #     elif n==2:
+        #         return 1
+        #     else:
+        #         return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3)
+        # return tribonacci(n)
