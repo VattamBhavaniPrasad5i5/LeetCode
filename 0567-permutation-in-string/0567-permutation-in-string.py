4871b367a601217cb1db4b314a35d6e2232b4f93
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2023-02-04 22:53:43 +0530

    Time: 1416 ms (23.74%), Space: 14.1 MB (28.54%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,7 +2,7 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         window = len(s1)
         s1_c = Counter(s1)
-        
+        print(s1_c,window)
         for i in range(len(s2)-window+1):
             s2_c = Counter(s2[i:i+window])
             if s2_c == s1_c:
