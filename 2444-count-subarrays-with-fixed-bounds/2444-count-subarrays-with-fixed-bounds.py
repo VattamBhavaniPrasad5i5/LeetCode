30aa0255c2f82592eec3abc1998b3d66a3df6bc7
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2023-03-05 00:51:11 +0530

    Time: 884 ms (76.53%), Space: 28.7 MB (41.16%) - LeetHub

---
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        result = 0
+        left_index = right_index = bad_index = -1
+        
+        for i, num in enumerate(nums):
+            if not minK <= num <= maxK:
+                bad_index = i
+            if num == minK:
+                left_index = i
+            if num == maxK:
+                right_index = i
+            result += max(0, min(left_index, right_index) - bad_index)
+        
+        return result
