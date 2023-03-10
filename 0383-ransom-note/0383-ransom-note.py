15d0afdf9eb76ac91e0d7382f0d3fd66c58d8844
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2023-03-11 01:08:26 +0530

    Time: 40 ms (91.94%), Space: 14.1 MB (89.59%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for i in ransomNote:
+            if i in magazine:
+                magazine=magazine.replace(i,"",1)
+                continue
+            else:
+                return False
+        return True
+        
