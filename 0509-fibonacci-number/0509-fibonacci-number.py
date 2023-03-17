78b66c6eac0910f73415e04f15126b9b85d680da
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2023-03-18 00:02:16 +0530

    Time: 799 ms (13.76%), Space: 13.9 MB (42.35%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -6,6 +6,9 @@ class Solution:
         # for x in range(2, n+1):
         #     fib1, fib2 = fib2, fib1 + fib2
         # return fib2
-        if n==1 or n==0:
+        # if n==1 or n==0:
+        #     return n
+        # return self.fib(n-1)+self.fib(n-2)
+        if n==0 or n==1:
             return n
         return self.fib(n-1)+self.fib(n-2)
