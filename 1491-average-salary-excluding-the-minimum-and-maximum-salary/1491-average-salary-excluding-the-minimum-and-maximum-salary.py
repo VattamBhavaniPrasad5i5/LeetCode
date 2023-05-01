742a62b7015385f02aef8b693415cb2c154a9776
Bhavani Prasad Vattam <97446586+VattamBhavaniPrasad5i5@users.noreply.github.com>
2023-05-01 21:42:46 +0530

    Time: 44 ms (5.49%), Space: 16.2 MB (8.21%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -1,4 +1,4 @@
 class Solution:
     def average(self, salary: List[int]) -> float:
-        print(sum(salary),max(salary)+min(salary))
+        #print(sum(salary),max(salary)+min(salary))
         return (sum(salary)-(max(salary)+min(salary)))/(len(salary)-2)
